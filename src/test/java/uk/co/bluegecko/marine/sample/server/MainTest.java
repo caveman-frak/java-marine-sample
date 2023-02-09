83b34fb4ee35c532d9da916567431f287c53f1f8
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-02-09 23:04:01 +0000

    project structure

---
+++ src/test/java/uk/co/bluegecko/marine/sample/server/MainTest.java
@@ -0,0 +1,20 @@
+package uk.co.bluegecko.marine.sample.server;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
