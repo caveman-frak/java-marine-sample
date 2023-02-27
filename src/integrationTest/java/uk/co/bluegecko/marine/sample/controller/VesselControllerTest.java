bd478aa1deddb7710d6b9e57bbb3136aadf262fd
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-02-27 21:08:31 +0000

    mock vessel service

--- src/integrationTest/java/uk/co/bluegecko/marine/sample/controller/VesselControllerTest.java
+++ src/integrationTest/java/uk/co/bluegecko/marine/sample/controller/VesselControllerTest.java
@@ -1,26 +1,48 @@
 package uk.co.bluegecko.marine.sample.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.co.bluegecko.marine.sample.handler.VesselHandler;
-import uk.co.bluegecko.marine.sample.service.base.VesselServiceBase;
+import uk.co.bluegecko.marine.sample.model.data.Vessel;
+import uk.co.bluegecko.marine.sample.service.VesselService;
 
+import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(VesselController.class)
-@ContextConfiguration(classes = {VesselController.class, VesselHandler.class, VesselServiceBase.class})
+@ContextConfiguration(classes = {VesselController.class, VesselHandler.class})
 public class VesselControllerTest {
+
+	@MockBean
+	private VesselService vesselService;
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() {
+		List<Vessel> vessels = List.of(
+				Vessel.builder().id(11, 1).name("Test 001").tonnage(1.0F).width(2.5F).length(5.1f).build(),
+				Vessel.builder().id(12, 2).active(false).name("Test 002").tonnage(1.1F).width(2.4F).length(5.2f).build(),
+				Vessel.builder().id(13, 3).name("Test 003").tonnage(1.2F).width(2.3f).length(5.3f).build(),
+				Vessel.builder().id(14, 4).name("Test 004").tonnage(1.3F).width(2.2f).length(5.4F).build(),
+				Vessel.builder().id(15, 5).name("Test 005").tonnage(1.4F).width(2.1F).length(5.5F).build()
+		);
+		when(vesselService.all()).thenReturn(vessels.stream().filter(Vessel::isActive).toList());
+		when(vesselService.find(any(UUID.class))).thenReturn(vessels.stream().findFirst());
+	}
 
 	@Test
 	void testGetAll() throws Exception {
