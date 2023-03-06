90f07f5a82bf75d3c0d7e7acbfb53a4b186a2a9e
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-03-06 09:23:49 +0000

    fix usage warnings

--- src/main/java/uk/co/bluegecko/marine/sample/controller/ControllerConstants.java
+++ src/main/java/uk/co/bluegecko/marine/sample/controller/ControllerConstants.java
@@ -1,5 +1,6 @@
 package uk.co.bluegecko.marine.sample.controller;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.web.servlet.function.RequestPredicate;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -9,8 +10,9 @@ import static org.springframework.web.servlet.function.RequestPredicates.accept;
 /**
  * Controller constants.
  */
+@UtilityClass
 public class ControllerConstants {
-	public static final RequestPredicate ACCEPT_JSON = accept(APPLICATION_JSON);
-	public static final RequestPredicate ACCEPT_XML = accept(APPLICATION_XML);
-	public static final RequestPredicate ACCEPT_JSON_XML = accept(APPLICATION_JSON, APPLICATION_XML);
+	public static RequestPredicate ACCEPT_JSON = accept(APPLICATION_JSON);
+	public static RequestPredicate ACCEPT_XML = accept(APPLICATION_XML);
+	public static RequestPredicate ACCEPT_JSON_XML = accept(APPLICATION_JSON, APPLICATION_XML);
 }
--- src/main/java/uk/co/bluegecko/marine/sample/service/base/VesselServiceBase.java
+++ src/main/java/uk/co/bluegecko/marine/sample/service/base/VesselServiceBase.java
@@ -26,13 +26,13 @@ public class VesselServiceBase implements VesselService {
 			Vessel.builder().id(11, 1).name("Test 001")
 					.tonnage(1.0, TONNE).width(2.5, METER).length(5.1, METER).build(),
 			Vessel.builder().id(12, 2).name("Test 002").active(false)
-					.tonnage(1.1, TONNE).width(2.4, METER).length(5.2, METER).build(),
+					.tonnage(1.1, TONNE).width(2.4, METER).width(2.5, METER).build(),
 			Vessel.builder().id(13, 3).name("Test 003")
 					.tonnage(1.2, TONNE).width(2.3, METER).length(5.3, METER).build(),
 			Vessel.builder().id(14, 4).name("Test 004")
-					.tonnage(1.3, TONNE).width(2.2, METER).length(5.4, METER).build(),
+					.tonnage(1.3, TONNE).width(2.2, METER).draught(1.4, METER).build(),
 			Vessel.builder().id(15, 5).name("Test 005")
-					.tonnage(1.4, TONNE).width(2.1, METER).length(5.5, METER).build()
+					.tonnage(1.4, TONNE).width(2.1, METER).length(5.5).build()
 	));
 
 	@Timed("vessel.all")
